2a48fd0be3c0aa9807f4285522bf5c89b582c3a3
Ian <iangoodwill2@gmail.com>
2020-04-21 21:00:52 -0600

    initial

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    
+    <Giphy/>
+    
+  </div>
+</template>
+
+<script>
+import Giphy from './components/Giphy.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Giphy
+  }
+}
+</script>
+
+<style>
+
+</style>
